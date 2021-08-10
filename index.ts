f340f1cf3f69ad15cf2229f5e2bc52a073df47bd
Soichiro Yoshimura <yoshimura@soichiro.org>
2021-08-10 22:41:29 +0900

    Delete lines

--- index.ts
+++ index.ts
@@ -6,8 +6,6 @@ const fs = require('fs').promises
     (async () => {
         const content = await fs.readFile('config.json', 'utf8');
         
-
-
         const browser = await puppeteer.launch({
             args: ['--no-sandbox', '--disable-setuid-sandbox'],
         });
