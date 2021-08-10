7d2b718d69b3daa5827315506d99acccc203930d
Soichiro Yoshimura <yoshimura@soichiro.org>
2021-08-10 22:42:18 +0900

    Delete import

--- index.ts
+++ index.ts
@@ -1,17 +1,13 @@
 import puppeteer, { Browser } from 'puppeteer';
-import { Page } from 'puppeteer';
-const fs = require('fs').promises
+const fs = require('fs').promises(async () => {
+  const content = await fs.readFile('config.json', 'utf8');
 
+  const browser = await puppeteer.launch({
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+  });
+  const page = await browser.newPage();
+  await page.goto('https://example.com');
+  await page.screenshot({ path: 'example.png' });
 
-    (async () => {
-        const content = await fs.readFile('config.json', 'utf8');
-        
-        const browser = await puppeteer.launch({
-            args: ['--no-sandbox', '--disable-setuid-sandbox'],
-        });
-        const page = await browser.newPage();
-        await page.goto('https://example.com');
-        await page.screenshot({ path: 'example.png' });
-
-        await browser.close();
-    })();
+  await browser.close();
+})();
