de1f5eae0d4b9522a1507626de5502969449fdfa
Soichiro Yoshimura <yoshimura@soichiro.org>
2021-08-10 22:40:00 +0900

    First Commit

---
+++ index.ts
@@ -0,0 +1,19 @@
+import puppeteer, { Browser } from 'puppeteer';
+import { Page } from 'puppeteer';
+const fs = require('fs').promises
+
+
+    (async () => {
+        const content = await fs.readFile('config.json', 'utf8');
+        
+
+
+        const browser = await puppeteer.launch({
+            args: ['--no-sandbox', '--disable-setuid-sandbox'],
+        });
+        const page = await browser.newPage();
+        await page.goto('https://example.com');
+        await page.screenshot({ path: 'example.png' });
+
+        await browser.close();
+    })();
